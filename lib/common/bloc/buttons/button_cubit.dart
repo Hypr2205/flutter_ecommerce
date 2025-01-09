8263ab70ca478da6b9502096c7f6877c7e4c7b2d
Hypr2205 <procango2003@gmail.com>
2025-01-09 23:14:10 +0700

    feat(buttons): Enhance ButtonCubit with execute method for feature handling
    - Added execute method to ButtonCubit for executing feature calls with dynamic parameters.
    - Implemented loading, success, and failure states to provide feedback during feature execution.
    - Utilized Either type from the dartz package for error handling and data management.

--- lib/common/bloc/buttons/button_cubit.dart
+++ lib/common/bloc/buttons/button_cubit.dart
@@ -1,8 +1,32 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_ecommerce/common/feature/feature.dart';
 import 'package:meta/meta.dart';
 
 part 'button_state.dart';
 
 class ButtonCubit extends Cubit<ButtonState> {
   ButtonCubit() : super(ButtonInitialState());
+
+  Future<void> execute({required Feature feature, dynamic params}) async {
+    emit(ButtonLoadingState());
+    try {
+      final _ = await feature.call(params: params) as Either
+        ..fold((error) {
+          emit(
+            ButtonFailState(
+              errorMessage: error as String,
+            ),
+          );
+        }, (data) {
+          emit(ButtonSuccessState());
+        });
+    } catch (e) {
+      emit(
+        ButtonFailState(
+          errorMessage: e.toString(),
+        ),
+      );
+    }
+  }
 }
