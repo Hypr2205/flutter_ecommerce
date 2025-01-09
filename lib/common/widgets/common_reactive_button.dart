7ec534e9377e265f3ac6639ac42423eb675464f7
Hypr2205 <procango2003@gmail.com>
2025-01-09 21:39:30 +0700

    feat(buttons): Add CommonReactiveButton widget for dynamic button states
    - Introduced CommonReactiveButton widget to handle button states using Bloc.
    - Integrated loading and initial states for better user feedback during interactions.
    - Updated common_widgets.dart to export the new CommonReactiveButton for accessibility.

---
+++ lib/common/widgets/common_reactive_button.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ecommerce/common/app_commons.dart';
+
+class CommonReactiveButton extends StatelessWidget {
+  const CommonReactiveButton({
+    required this.onPressed,
+    this.title = '',
+    this.height,
+    this.content,
+    super.key,
+  });
+  final VoidCallback onPressed;
+  final String title;
+  final double? height;
+  final Widget? content;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ButtonCubit, ButtonState>(
+      builder: (context, state) {
+        if (state is ButtonLoadingState) {
+          return _loading();
+        }
+        return _initial();
+      },
+    );
+  }
+
+  Widget _loading() {
+    return ElevatedButton(
+      onPressed: null,
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size.fromHeight(height ?? 50),
+      ),
+      child: Container(
+        height: height ?? 50,
+        alignment: Alignment.center,
+        child: const CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  Widget _initial() {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size.fromHeight(height ?? 50),
+      ),
+      child: content ??
+          Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+    );
+  }
+}
--- lib/common/widgets/common_widgets.dart
+++ lib/common/widgets/common_widgets.dart
@@ -1,2 +1,3 @@
 export 'common_app_bar.dart';
 export 'common_button.dart';
+export 'common_reactive_button.dart';
